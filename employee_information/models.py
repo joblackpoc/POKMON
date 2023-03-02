4a6ce1cbb129bc9c1ee2b18e83af6ddbb874ae8d
joblackpoc <duopoc@gmail.com>
2023-03-02 22:11:18 +0700

    POPMON EMS

---
+++ employee_information/models.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Department(models.Model):
+    name = models.TextField() 
+    description = models.TextField() 
+    status = models.IntegerField() 
+    date_added = models.DateTimeField(default=timezone.now) 
+    date_updated = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return self.name
+
+class Position(models.Model):
+    name = models.TextField() 
+    description = models.TextField() 
+    status = models.IntegerField() 
+    date_added = models.DateTimeField(default=timezone.now) 
+    date_updated = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return self.name
+
+
+class Employees(models.Model):
+
+    firstname = models.TextField() 
+
+    lastname = models.TextField() 
+    gender = models.TextField(blank=True,null= True) 
+
+    contact = models.TextField() 
+    address = models.TextField() 
+
+    department_id = models.ForeignKey(Department, on_delete=models.CASCADE) 
+    position_id = models.ForeignKey(Position, on_delete=models.CASCADE) 
+    date_hired = models.DateField() 
+
+    status = models.IntegerField() 
+    date_added = models.DateTimeField(default=timezone.now) 
+    date_updated = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return self.firstname + ' - '+self.lastname + ' '
