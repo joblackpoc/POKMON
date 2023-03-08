fd9b64b6357c2e727066af2a5887da4b486fd490
joblackpoc <duopoc@gmail.com>
2023-03-09 01:16:18 +0700

    modified model

--- employee_information/models.py
+++ employee_information/models.py
@@ -4,21 +4,21 @@ from django.utils import timezone
 
 # Create your models here.
 class Department(models.Model):
-    name = models.TextField() 
-    description = models.TextField() 
-    status = models.IntegerField() 
-    date_added = models.DateTimeField(default=timezone.now) 
-    date_updated = models.DateTimeField(auto_now=True) 
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.IntegerField()
+    date_added = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
 class Position(models.Model):
-    name = models.TextField() 
-    description = models.TextField() 
-    status = models.IntegerField() 
-    date_added = models.DateTimeField(default=timezone.now) 
-    date_updated = models.DateTimeField(auto_now=True) 
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.IntegerField()
+    date_added = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -26,13 +26,13 @@ class Position(models.Model):
 
 class Employees(models.Model):
 
-    firstname = models.TextField() 
+    firstname = models.CharField(max_length=200,) 
 
-    lastname = models.TextField() 
-    gender = models.TextField(blank=True,null= True) 
+    lastname = models.CharField(max_length=200,) 
+    gender = models.CharField(max_length=200, blank=True,null=True) 
 
-    contact = models.TextField() 
-    address = models.TextField() 
+    contact = models.CharField(max_length=200,) 
+    address = models.CharField(max_length=200,) 
 
     department_id = models.ForeignKey(Department, on_delete=models.CASCADE) 
     position_id = models.ForeignKey(Position, on_delete=models.CASCADE) 
